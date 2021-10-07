aec29ee1c3f2f2f1beea8544a43686832555f415
Pravin <psavkar@yahoo.com>
2021-10-06 23:21:09 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,39 @@
-module.exports = defineComponent({
+import Airtable from "airtable"
+
+export default defineComponent({
+  props: {
+    airtable: {
+      type: "app",
+      app: "airtable",
+    },
+    baseId: {
+      type: "$.airtable.baseId",
+      appProp: "airtable",
+    },
+    tableId: {
+      type: "$.airtable.tableId",
+      baseIdProp: "baseId",
+    },
+  },
   async run({steps, $}) {
-    return "foo"
+    const base = new Airtable({apiKey: this.airtable.$auth.api_key}).base(this.baseId);
+
+    const data = []
+    
+    await base(this.tableId).select({
+      // pass optional config parameters here
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        records.forEach(function(record) {
+            data.push(record._rawJson)
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    })
+
+    return data
   },
 })
